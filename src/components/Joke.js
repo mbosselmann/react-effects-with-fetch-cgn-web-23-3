fc52da647482322a323c535623cdd5ede9495ff2
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-11-17 10:20:27 +0100

    Implement fetch functionality

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react";
+
 export default function Joke() {
+  const [joke, setJoke] = useState(null);
+  const [id, setId] = useState(0);
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        `https://example-apis.vercel.app/api/bad-jokes/${id}`
+      );
+      const joke = await response.json();
+      setJoke(joke);
+    }
+
+    startFetching();
+  }, [id]);
+
+  if (!joke) {
+    // return null;
+    // return;
+    return <p>Nothing to see here yet.</p>;
+  }
+
   return (
     <>
-      <h1>Insert joke here</h1>
-      <button type="button">Next Joke</button>
+      <h1>{joke.joke}</h1>
+      <button type="button" onClick={() => setId(joke.nextId)}>
+        Next Joke
+      </button>
     </>
   );
 }
