a9ae4d0e380678faca3669361befe5ae4a9d83f9
Mareike Boßelmann <mareike.bosselmann@gmx.de>
2023-11-17 11:02:09 +0100

    Implement random joke functionality

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,30 +2,27 @@ import { useState, useEffect } from "react";
 
 export default function Joke() {
   const [joke, setJoke] = useState(null);
-  const [id, setId] = useState(0);
+
+  async function getRandomJoke() {
+    const response = await fetch(
+      `https://example-apis.vercel.app/api/bad-jokes/random`
+    );
+    const joke = await response.json();
+    setJoke(joke);
+  }
 
   useEffect(() => {
-    async function startFetching() {
-      const response = await fetch(
-        `https://example-apis.vercel.app/api/bad-jokes/${id}`
-      );
-      const joke = await response.json();
-      setJoke(joke);
-    }
-
-    startFetching();
-  }, [id]);
+    getRandomJoke();
+  }, []);
 
   if (!joke) {
-    // return null;
-    // return;
-    return <p>Nothing to see here yet.</p>;
+    return null;
   }
 
   return (
     <>
       <h1>{joke.joke}</h1>
-      <button type="button" onClick={() => setId(joke.nextId)}>
+      <button type="button" onClick={() => getRandomJoke()}>
         Next Joke
       </button>
     </>
